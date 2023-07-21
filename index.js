6b037cf4e8f31161b0fe4db786303f610a8c0b87
brmlee <brmlee@umich.edu>
2023-07-20 20:31:52 -0400

    js,html, files

---
+++ index.js
@@ -0,0 +1,10 @@
+ReactDOM.render(<h1>Hello Everyone</h1>, document.getElementById("root"))
+ReactDOM.render(<ul><li>Thing 1</li><li>Thing 2</li></ul>, document.getElementById("root1"))
+
+
+// Clear the existing HTML content
+// document.body.innerHTML = '<div id="app"></div>';
+
+// // Render your React component instead
+// const root = createRoot(document.getElementById('app'));
+// root.render(<h1>Hello to the moon and back</h1>);
